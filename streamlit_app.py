cc44d2179fd88f1fb0905dea9b5210f643ca5968
ElzbietaDz <elzbieta.dziurowicz@gmail.com>
2024-05-07 15:31:34 +0200

    Update streamlit_app.py
    Nutrition information for chosen fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 #list of fuit
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -31,7 +30,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
         fv_df =  st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
